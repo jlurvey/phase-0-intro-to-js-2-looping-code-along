0ff76e881d01a949aa24bc98350709fb63a9b114
Joseph Lurvey <joeylurvey@gmail.com>
2023-05-03 20:05:47 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    const newArray = [];
+    for (let i = 0; i < names.length; i++) {
+        newArray.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    };
+    return newArray;
+};
+
+function countDown(integer) {
+    while (integer >= 0) {
+        console.log(integer);
+        integer--;
+    };
+};
